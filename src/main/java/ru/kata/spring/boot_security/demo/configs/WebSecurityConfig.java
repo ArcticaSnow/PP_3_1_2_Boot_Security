acae5ad4d1d82556006fbc33eb9b2a82113a5674
ArcticaSnow <katy.shamina@yandex.ru>
2022-11-06 12:33:32 +0300

    Small fix

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -22,13 +22,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()// ?
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated()
-                .and()
-                .httpBasic()
                 .and()
                 .formLogin().successHandler(successUserHandler)
                 .permitAll()
